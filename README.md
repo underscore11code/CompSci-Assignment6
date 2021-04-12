9fcb534d08758f284dfa1f5d2369079d8a42fc46
underscore11code <minecrafter11mrt@gmail.com>
2021-04-12 09:36:39 -0700

    dictionary

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # CompSci
 A bunch of Java for usage in my Comp Sci class. Expect messy code.
+
+## Instructions: 
+
+Create a Dictionary application which prompts the user for minimum of 3 unidentical words and display the words in alphabetical order. On the next line, ask the user if they want to run the program again. When the user agrees to run the program again, repeat the process, otherwise display “Thank you for using the dictionary.”
+
+## Program Run Sketch:
+
+```
+Do you want to start using the dictionary (yes/no)? yes
+
+Enter a word (check your spelling carefully): tiger
+
+Enter another word (check your spelling carefully): Elephant
+
+Enter one more word (check your spelling carefully): zebra
+
+You have entered the following words:
+ELEPHANT
+TIGER
+ZEBRA
+
+Would you like to use the dictionary again (yes/no)? no
+
+Thank you for using the dictionary.
+```
+
+### Tips:
+
+Make sure you correctly initialize and close the Scanner class.
+
+Make sure you use the most efficient loop structure.
+
+Note the display of user input words are in UPPERCASE. 
--- src/main/java/io/github/underscore11code/compsci/Main.java
+++ src/main/java/io/github/underscore11code/compsci/Main.java
@@ -1,7 +1,51 @@
 package io.github.underscore11code.compsci;
 
-public class Main {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main implements Runnable {
+  private Set<String> words = new HashSet<>();
+  private Scanner scanner = new Scanner(System.in);
+
   public static void main(String[] args) {
-    // Do shit here
+    new Main().run();
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      final String word = this.prompt("Enter a word, or \"done\" if you are done:",
+              String::toUpperCase,
+              s -> !s.equals("") && !words.contains(s));
+      if (word.equalsIgnoreCase("done")) break;
+      words.add(word);
+    }
+    System.out.println(words.stream().sorted().collect(Collectors.toList()));
+  }
+
+  private String prompt(final String prompt, final Predicate<String> check) {
+    while (true) {
+      System.out.print(prompt);
+      final String line = this.scanner.nextLine();
+      if (check.test(line)) return line;
+      System.out.println("Invalid input.");
+    }
+  }
+
+  private String prompt(final String prompt, final String... options) {
+    return this.prompt(prompt, p -> Arrays.asList(options).contains(p));
+  }
+
+  private <T> T prompt(final String prompt, final Function<String, T> map, final Predicate<String> check) {
+    return map.apply(this.prompt(prompt, check));
+  }
+
+  private <T> T prompt(final String prompt, final Function<String, T> map, final String... options) {
+    return map.apply(this.prompt(prompt, options));
   }
 }
